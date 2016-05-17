69b47df5e009bc0377de3e608ad7f9364d6ee11c
Greg Kessman <greg.kessman@gmail.com>
2016-05-17 17:12:37 -0400

    Project structure changes, add date/time logging

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.redirect('/index.html');
 });
 
 io.on('connection', function(socket) {
-	console.log('a user connected');
-	io.emit('event message', 'a user connected');
+	console.log('User connected');
+	io.emit('event message', 'User connected');
 	socket.on('disconnect', function() {
-		console.log('user disconnected');
-		io.emit('event message', 'a user disconnected');
+		console.log('User disconnected');
+		io.emit('event message', 'User disconnected');
 	});
 
 	socket.on('chat message', function(msg) {
@@ -25,6 +29,6 @@ io.on('connection', function(socket) {
 	});
 });
 
-http.listen(3000, function() {
-	console.log('listening on port 3000');
+http.listen(port, function() {
+	console.log('listening on port ' + port);
 });
