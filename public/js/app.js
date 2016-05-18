8092c55d54dc5eba88e66d8aa9f781a498e4d0d8
Greg Kessman <greg.kessman@gmail.com>
2016-05-18 13:01:40 -0400

    change classes to id's for unifority

--- public/js/app.js
+++ public/js/app.js
@@ -17,19 +17,19 @@ $('form').submit(function() {
 socket.on('chat message', function(msg) {
 	checkDate();
 	$('#messages').append($('<li>').text(msg.body));
-	$('#messages li:last').prepend($('<p class="name">').text(msg.user));
-	$('.name:last').css('color', msg.nameColor)
-	$('#messages li:last').append($('<p class="time">').text(new Date(msg.time).toLocaleTimeString()));
+	$('#messages li:last').prepend($('<p id="name">').text(msg.user));
+	$('#name:last-child').css('color', msg.nameColor)
+	$('#messages li:last').append($('<p id="time">').text(new Date(msg.time).toLocaleTimeString()));
 });
 
 socket.on('event message', function(event) {
 	checkDate();
-	$('#messages').append($('<li class="event">').text(event.user + ' ' + event.action));
-	$('#messages li:last').append($('<p class="time">').text(new Date().toLocaleTimeString()));
+	$('#messages').append($('<li id="event">').text(event.user + ' ' + event.action));
+	$('#messages li:last').append($('<p id="time">').text(new Date().toLocaleTimeString()));
 	if (!name) {
 		name = event.user;
 		nameColor = event.nameColor;
-		$('.user').text(name).css('text-shadow', '1px 1px 2px black, 0 0 10px ' + nameColor);
+		$('#user').text(name).css('text-shadow', '1px 1px 2px black, 0 0 10px ' + nameColor);
 	}
 });
 
@@ -38,6 +38,6 @@ var checkDate = function() {
 	var date = d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear();
 	if (dateBanner != date) {
 		dateBanner = date;
-		$('#messages').append($('<li class="date">').text(dateBanner));
+		$('#messages').append($('<li id="date">').text(dateBanner));
 	}
 }
